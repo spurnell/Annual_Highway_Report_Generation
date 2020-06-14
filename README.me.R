b9c975f28d6d51d823fc5caaa632f889dadf174e
spencepurnell <spence.purnell@gmail.com>
2020-06-14 10:53:44 -0400

    README.me

---
+++ README.me.R
@@ -0,0 +1,5 @@
+README.me 
+
+#These are the steps taken to generate the proprietary calculated metrics
+#Data collection from FHWA and other sources, as well as congestion, is conducted elsewhere
+
